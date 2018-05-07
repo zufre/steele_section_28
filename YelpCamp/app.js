a7b168f8035ffed68025ba88a453f987cf9d9be6
User <user@Mac.local>
2018-05-07 13:19:55 +1000

    added mongoose to yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,42 +1,74 @@
-var express = require("express");
-var app = express();
-var bodyparser = require("body-parser");
+var express       = require("express"),
+    app           = express(),
+    bodyparser    = require("body-parser"),
+    mongoose      = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-var campgrounds =[
-  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
-  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
-  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
-  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
-  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
-  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
-  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
-  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
-  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
-  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
-  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
-  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'}
 
-  ]
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema );
+
+// Campground.create(
+//   {       name: 'Granite Hill', 
+//           image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'
+//   }, function(err, campground) {
+//     if(err) {
+//       console.log("error");
+//     }else{
+//       console.log("newly create campground");
+//       console.log(campground)
+//     }
+//   });
+
+//var campgrounds =[
+  // {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  // {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  // {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
+  // {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  // {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  // {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
+  // {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  // {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  // {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
+  // {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  // {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  // {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'}
+
+  // ]
 
 app.get('/', function(req, res) {
   res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-  res.render('campgrounds',{campgrounds:campgrounds});  
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log("error");  
+    }else{
+      res.render('campgrounds',{campgrounds:allCampgrounds});  
+    }
+  });
 });
 
 app.post('/campgrounds', function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
-
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err) {
+      console.log(err);
+    }else{
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get('/campgrounds/new', function(req, res) {
